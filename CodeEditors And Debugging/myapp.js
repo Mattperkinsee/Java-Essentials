1fb9cc76e9c347bdcebf369f8789143e656b65cb
Matthew Perkins <mattperkinsee@gmail.com>
2017-11-01 07:40:14 -0400

    Initial commit

---
+++ CodeEditors And Debugging/myapp.js
@@ -0,0 +1,20 @@
+var car = {
+        
+       make: "chevy",
+        speed: 200,
+        engine:{
+            size:2.0,
+            make: "bmw",
+            fuel:"petrol",
+            pistons: ["piston1","piston2"]
+        }
+        
+    };
+
+
+var shoppingList = [
+    "Apple",
+    "orange",
+    "Pear"
+    
+];
